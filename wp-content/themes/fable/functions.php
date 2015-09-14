4b8c2efd9a7958a5b759b822537b028dada4cc52
annguyenit <annguyenphp@gmail.com>
2015-09-15 01:15:29 +0700

    Update function.php: add filter action

--- wp-content/themes/fable/functions.php
+++ wp-content/themes/fable/functions.php
@@ -13,5 +13,12 @@ else require_once('public/functions.php');
 add_action('after_setup_theme',array($Theme,'setupTheme'));
 add_action('switch_theme',array($Theme,'switchTheme'));
 
+add_filter( 'wpcf7_form_class_attr', 'he_wpcf7_add_class_attr' );
+
+function he_wpcf7_add_class_attr( $class ) {
+	$class .= ' pb-contact-form';
+	return $class;
+}
+
 /******************************************************************************/
 /******************************************************************************/
